cd8cd9ebf21bb9b9d4a75adbd6b8a28fa349505f
dka77877 <63919331+dka77877@users.noreply.github.com>
2020-08-09 21:00:45 +0300

    Add files via upload

---
+++ Script_dz6.sql
@@ -0,0 +1,166 @@
+-- Вариант 10 (финальный)
+-- Применим вариант с таблицей типов лайков
+-- (применяем к базе vk только этот вариант!)
+
+-- Таблица лайков
+DROP TABLE IF EXISTS likes;
+CREATE TABLE likes (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user_id INT UNSIGNED NOT NULL,
+  target_id INT UNSIGNED NOT NULL,
+  target_type_id INT UNSIGNED NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Таблица типов лайков
+DROP TABLE IF EXISTS target_types;
+CREATE TABLE target_types (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL UNIQUE,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO target_types (name) VALUES 
+  ('messages'),
+  ('users'),
+  ('media'),
+  ('posts');
+
+-- Заполняем лайки
+INSERT INTO likes 
+  SELECT 
+    id, 
+    FLOOR(1 + (RAND() * 100)), 
+    FLOOR(1 + (RAND() * 100)),
+    FLOOR(1 + (RAND() * 4)),
+    CURRENT_TIMESTAMP 
+  FROM messages;
+
+-- Проверим
+SELECT * FROM likes LIMIT 10;
+
+-- Создадим таблицу постов
+CREATE TABLE posts (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user_id INT UNSIGNED NOT NULL,
+  community_id INT UNSIGNED,
+  head VARCHAR(255),
+  body TEXT NOT NULL,
+  media_id INT UNSIGNED,
+  is_public BOOLEAN DEFAULT TRUE,
+  is_archived BOOLEAN DEFAULT FALSE,
+  views_counter INT UNSIGNED DEFAULT 0,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+
+-- Добавляем внешние ключи в БД vk
+-- Для таблицы профилей
+
+-- Смотрим структуру таблицы
+DESC profiles;
+
+-- Добавляем внешние ключи
+ALTER TABLE profiles
+  ADD CONSTRAINT profiles_user_id_fk 
+    FOREIGN KEY (user_id) REFERENCES users(id)
+      ON DELETE CASCADE,
+  ADD CONSTRAINT profiles_photo_id_fk
+    FOREIGN KEY (photo_id) REFERENCES media(id)
+      ON DELETE SET NULL;
+
+-- Изменяем тип столбца при необходимости
+ALTER TABLE profiles DROP FOREIGN KEY profiles_user_id_fk;
+ALTER TABLE profiles MODIFY COLUMN photo_id INT(10) UNSIGNED;
+      
+-- Для таблицы сообщений
+
+-- Смотрим структурв таблицы
+DESC messages;
+
+-- Добавляем внешние ключи
+ALTER TABLE messages
+  ADD CONSTRAINT messages_from_user_id_fk 
+    FOREIGN KEY (from_user_id) REFERENCES users(id),
+  ADD CONSTRAINT messages_to_user_id_fk 
+    FOREIGN KEY (to_user_id) REFERENCES users(id);
+
+-- Смотрим диаграмму отношений в DBeaver (ERDiagram)
+
+ALTER TABLE profiles 
+	ADD CONSTRAINT profiles_user_id_fk
+		FOREIGN KEY (user_id) REFERENCES users(id)
+			ON DELETE CASCADE,
+	ADD CONSTRAINT profiles_photo_id_fk
+		FOREIGN KEY (photo_id) REFERENCES media(id)
+			ON DELETE SET NULL;	
+	
+ALTER TABLE communities_users 
+	ADD CONSTRAINT communities_users_community_id_fk
+		FOREIGN KEY (community_id) REFERENCES communities(id)
+			ON DELETE CASCADE,
+	ADD CONSTRAINT communities_users_user_id_fk
+		FOREIGN KEY (user_id) REFERENCES users(id)
+			ON DELETE CASCADE;		
+
+ALTER TABLE friendship 
+	ADD CONSTRAINT friendship_status_id_fk
+		FOREIGN KEY (status_id) REFERENCES friendship_statuses(id)
+			ON DELETE CASCADE,
+	ADD CONSTRAINT friendship_user_id_fk
+		FOREIGN KEY (user_id) REFERENCES users(id)
+			ON DELETE CASCADE,	
+	ADD CONSTRAINT friendship_friend_id_fk
+		FOREIGN KEY (friend_id) REFERENCES users(id)
+			ON DELETE CASCADE;		
+			
+ALTER TABLE likes 
+	ADD CONSTRAINT likes_target_type_id_fk
+		FOREIGN KEY (target_type_id) REFERENCES target_types(id)
+			ON DELETE CASCADE,
+	ADD CONSTRAINT likes_user_id_fk
+		FOREIGN KEY (user_id) REFERENCES users(id)
+			ON DELETE SET NULL;
+			
+ALTER TABLE posts 
+	ADD CONSTRAINT post_media_id_fk
+		FOREIGN KEY (media_id) REFERENCES media(id)
+			ON DELETE SET NULL,
+	ADD CONSTRAINT post_community_id_fk
+		FOREIGN KEY (community_id) REFERENCES communities(id)
+			ON DELETE CASCADE,
+	ADD CONSTRAINT post_user_id_fk
+		FOREIGN KEY (user_id) REFERENCES users(id)
+			ON DELETE SET NULL;
+			
+SELECT
+	(SELECT gender FROM profiles WHERE user_id = likes.user_id) AS gender,
+	COUNT(*) AS total
+    FROM likes
+    GROUP BY gender
+    ORDER BY total DESC
+    
+SELECT 
+  (SELECT COUNT(*) FROM likes WHERE target_id = profiles.user_id AND target_type_id = 2) AS likes_total  
+  FROM profiles 
+  ORDER BY birthday 
+  DESC LIMIT 10;
+    
+SELECT SUM(likes_total) FROM  
+  (SELECT 
+    (SELECT COUNT(*) FROM likes WHERE target_id = profiles.user_id AND target_type_id = 2) AS likes_total  
+    FROM profiles 
+    ORDER BY birthday 
+    DESC LIMIT 10) AS user_likes;
+   
+SELECT 
+  CONCAT(first_name, ' ', last_name) AS user, 
+	(SELECT COUNT(*) FROM likes WHERE likes.user_id = users.id) + 
+	(SELECT COUNT(*) FROM media WHERE media.user_id = users.id) + 
+	(SELECT COUNT(*) FROM messages WHERE messages.from_user_id = users.id) AS overall_activity 
+	  FROM users
+	  ORDER BY overall_activity
+	  LIMIT 10;
+    
+    
